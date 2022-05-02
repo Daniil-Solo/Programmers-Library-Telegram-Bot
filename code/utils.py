520db4c72e294b1a14fb4902632e97bdcafe00ee
Daniil-Solo <daniil.solo1723@gmail.com>
2022-05-03 00:36:15 +0500

    add decorator for connection

--- code/utils.py
+++ code/utils.py
@@ -21,20 +21,36 @@ def save_book(book_info: str):
     create_topic_book_relation(book_id, topic_id_list)
 
 
-def get_or_create_author_id(author: str) -> int:
+def connection_decorator(function):
+    """
+    Данный декоратор оборачивает функцию для работы с БД
+    Отвечает за установку соединения с базой и закрытием этого соединения
+    """
+    def wrapped(*args, **kwargs):
+        configs = get_configs()
+        connection = psycopg2.connect(user=configs.get('user'),
+                                      password=configs.get('password'),
+                                      host=configs.get('host'),
+                                      port=configs.get('port'),
+                                      database=configs.get('database'))
+        cursor = connection.cursor()
+
+        result = function(*args, cursor=cursor, connection=connection, **kwargs)
+
+        connection.close()
+        cursor.close()
+        return result
+    return wrapped
+
+
+@connection_decorator
+def get_or_create_author_id(author: str, **kwargs) -> int:
     """
     Обращается к базе данных за id указанного автора
     Если такого нет, то создается новая запись
     Возвращает id автора
     """
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
-
+    cursor, connection = kwargs.get('cursor'), kwargs.get('connection')
     get_query = f"""
         SELECT id, full_name
         FROM author
@@ -54,24 +70,17 @@ def get_or_create_author_id(author: str) -> int:
         cursor.execute(get_query)
         result = cursor.fetchone()
 
-    connection.close()
-    cursor.close()
     return result[0]
 
 
-def get_and_create_book_id(bt: dict) -> int:
+@connection_decorator
+def get_and_create_book_id(bt: dict, **kwargs) -> int:
     """
     Обращается к базе данных за id указанной книги
     Создается новая запись, не учитывается ошибка уникальности
     Возвращает id книги
     """
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
+    cursor, connection = kwargs.get('cursor'), kwargs.get('connection')
     get_query = f"""
         SELECT id, title
         FROM book 
@@ -87,25 +96,17 @@ def get_and_create_book_id(bt: dict) -> int:
     cursor.execute(get_query)
     result = cursor.fetchone()
 
-    connection.close()
-    cursor.close()
     return result[0]
 
 
-def get_and_create_topic_id_list(topics: list):
+@connection_decorator
+def get_and_create_topic_id_list(topics: list, **kwargs):
     """
     Обращается к базе данных за id указанных тем
     Если такой темы нет, то создается новая запись
     Возвращает список, содержащий id тем
     """
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
-
+    cursor, connection = kwargs.get('cursor'), kwargs.get('connection')
     topic_id_list = []
     for topic in topics:
         create_query = f"""
@@ -126,24 +127,16 @@ def get_and_create_topic_id_list(topics: list):
             cursor.execute(get_query)
             result = cursor.fetchone()
         topic_id_list.append(result[0])
-
-    connection.close()
-    cursor.close()
     return topic_id_list
 
 
-def create_topic_book_relation(book_id: int, topic_id_list: list) -> None:
+@connection_decorator
+def create_topic_book_relation(book_id: int, topic_id_list: list, **kwargs) -> None:
     """
     Обращается к базе данных для создания связи между книгой и темами
     Ничего не возвращает
     """
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
+    cursor, connection = kwargs.get('cursor'), kwargs.get('connection')
     for topic_id in topic_id_list:
         create_query = f"""
         INSERT INTO topic_book(book_id, topic_id)
@@ -152,19 +145,13 @@ def create_topic_book_relation(book_id: int, topic_id_list: list) -> None:
         cursor.execute(create_query)
         connection.commit()
 
-    connection.close()
-    cursor.close()
 
-
-def get_file_id(book_id: int) -> tuple:
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
-
+@connection_decorator
+def get_file_id(book_id: int, **kwargs) -> tuple:
+    """
+    Возвращает id файла по указанному id книги
+    """
+    cursor = kwargs.get('cursor')
     get_query = f"""
         SELECT file_id
         FROM book
@@ -172,43 +159,31 @@ def get_file_id(book_id: int) -> tuple:
         """
     cursor.execute(get_query)
     result = cursor.fetchone()
-
-    connection.close()
-    cursor.close()
     return result
 
 
-def find_book_in_db_by_title(text: str) -> list:
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
-
-    get_query = f"""
-    SELECT title, id
-    FROM book
-    WHERE title ILIKE '%{text}%'
+@connection_decorator
+def find_book_in_db_by_title(text: str, **kwargs) -> list:
     """
+    Возвращает книги с названием, содержащим указанный текст
+    """
+    cursor = kwargs.get('cursor')
+    get_query = f"""
+        SELECT title, id
+        FROM book
+        WHERE title ILIKE '%{text}%'
+        """
     cursor.execute(get_query)
     result = cursor.fetchall()
-
-    connection.close()
-    cursor.close()
     return result
 
 
-def find_book_in_db_by_author(text: str) -> list:
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
-
+@connection_decorator
+def find_book_in_db_by_author(text: str, **kwargs) -> list:
+    """
+        Возвращает книги с именем автора, содержащим указанный текст
+        """
+    cursor = kwargs.get('cursor')
     get_query = f"""
         SELECT book.title, book.id
         FROM book
@@ -218,21 +193,15 @@ def find_book_in_db_by_author(text: str) -> list:
         """
     cursor.execute(get_query)
     result = cursor.fetchall()
-
-    connection.close()
-    cursor.close()
     return result
 
 
-def find_book_in_db_by_keywords(keywords: list) -> list:
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
-
+@connection_decorator
+def find_book_in_db_by_keywords(keywords: list, **kwargs) -> list:
+    """
+    Возвращает все книги, для которых есть подходящие ключевые слова
+    """
+    cursor = kwargs.get('cursor')
     topics = set()
     for key in keywords:
         check_query = f"""
@@ -259,21 +228,15 @@ def find_book_in_db_by_keywords(keywords: list) -> list:
 
         cursor.execute(get_query)
         result = cursor.fetchall()
-
-    connection.close()
-    cursor.close()
     return result
 
 
-def find_last_books() -> list:
-    configs = get_configs()
-    connection = psycopg2.connect(user=configs.get('user'),
-                                  password=configs.get('password'),
-                                  host=configs.get('host'),
-                                  port=configs.get('port'),
-                                  database=configs.get('database'))
-    cursor = connection.cursor()
-
+@connection_decorator
+def find_last_books(**kwargs) -> list:
+    """
+    Возвращает последние добавленные книги
+    """
+    cursor = kwargs.get('cursor')
     get_query = f"""
         SELECT title, id
         FROM book
@@ -282,7 +245,4 @@ def find_last_books() -> list:
         """
     cursor.execute(get_query)
     result = cursor.fetchall()
-
-    connection.close()
-    cursor.close()
     return result
